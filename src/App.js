74067660cd360a485c9f74a5d17ee8890b361834
Jamboysen <peregrinojvepsocmeds@gmail.com>
2024-07-18 14:41:09 +0800

    John Vincent Peregrino

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
   <div>
     <nav>
       <Link to="/RyojiBernardo" className="nav-item">Ryoji Bernardo</Link>
+      <Link to="/JohnVincentPeregrino" className="nav-item">John Vincent Peregrino</Link>
     </nav>
     <Routes>
       <Route path="/RyojiBernardo" element={<Hero name="Ryoji Bernardo" SN="2021107273"/>} />
+      <Route path="/JohnVincentPeregrino" element={<Hero name="John Vincent Peregrino" SN="2020115902"/>} />
     </Routes>
   </div>
   );
